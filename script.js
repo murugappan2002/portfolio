24d9df4fbd8ef486d7249d197adbff2eea7dd593
murugappan2002 <154869485+murugappan2002@users.noreply.github.com>
2023-12-28 12:53:27 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+// var name="muru";
+// var age=4;
+// let bool=true;
+
+// function printName(name,age,bool){
+// let vf = 6
+// console.log("name:",name);
+// console.log("age:",age);
+// console.log("bool:",bool);
+// console.log("vf",vf);
+// }
+// printName(name,age,bool);
+
+// // console.log("vfu",vf);
+
+// //billing sorvices
+// let bill=50000;
+// let discount=10;
+// let tax=5; 
+// function Bill(bill,discount,tax){
+//     let totalbil=bill-(bill*discount/100)+(bill*100)
+//     console.log(totalbil);
+// }
+// Bill(bill,discount,tax)
+//function addtask(){
+    //let task = document.getElementById("takeinput").value;
+    //console.log(task);
+    //if (task){
+        //et li=document.createElement("li");
+        //li.textContent = task;
+        //document.getElementById( "m2").appendChild(li);
+       // document.getElementById("takeinput").value="";
+        //li.onclick = function(){
+        //    thid.parentNode.removeChild(this);
+       // }
+     //}
+//}
+<body>
+    <div>
+        <select id="item"onchange="bg()">
+
+    </div>
+</body>
